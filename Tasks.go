95f9963b4c351344bcbce1ab253a54fc21d0d24d
Areeba T. Shoaib <areebashoaib99@gmail.com>
2022-07-14 19:35:48 +0200

    feat(Tasks.go) added_math_package

--- Tasks.go
+++ Tasks.go
@@ -1,21 +1,24 @@
 package main
 
-import "fmt"
-
-
-
-
+import (
+	"fmt"
+	"math"
+)
 
 func add(x int, y int) int {
-	var out=x+y
+	var out = x + y
 	return out
 
 }
 
 func main() {
-	var num1 int =3
-	var num2 int =5
+	var num1 int = 3
+	var num2 int = 5
 
-	fmt.Println(add(num1,num2))
+	var num3 float64 = 9
+	var result = math.Sqrt(num3)
+
+	fmt.Println(add(num1, num2))
+	fmt.Println(result)
 
 }
