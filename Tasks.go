beb31b25d034d3a4fc7dd5d5e0007c7c73a6562e
Areeba T. Shoaib <areebashoaib99@gmail.com>
2022-07-14 18:58:33 +0200

    feat(Tasks.go) practiced function

---
+++ Tasks.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+
+
+
+
+func add(x int, y int) int {
+	var out=x+y
+	return out
+
+}
+
+func main() {
+	var num1 int =3
+	var num2 int =5
+
+	fmt.Println(add(num1,num2))
+
+}
