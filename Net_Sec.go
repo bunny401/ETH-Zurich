d12be3d7c75f9008da547c812302601bbb409ce4
Areeba T. Shoaib <areebashoaib99@gmail.com>
2022-07-24 14:04:14 +0200

    Added Channel

--- Net_Sec.go
+++ Net_Sec.go
@@ -1,11 +1,28 @@
 package main
 import ("fmt")
-
+//Created a structure
 type Packet struct{
 	Payload byte
 	Type int
 }
 
+func get(Type, ch chan Packet){
+	
+ 
+
+}
+
+
+
+
+
+func main(){
+	//Created a channel for packets
+	packets_Channel := make(chan Packet)
+
+
+
+}
 
 
 
