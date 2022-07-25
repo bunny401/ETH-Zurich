02ecfc441be100d4d857449c822c62a62d1193a2
Areeba T. Shoaib <areebashoaib99@gmail.com>
2022-07-25 16:08:15 +0200

    Added gorotuines

--- Udemy_first_challenge.go
+++ Udemy_first_challenge.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 var msg string
 
 func updateMessage(s string) {
+	defer wg.Done()
 
 	msg = s
 }
@@ -14,6 +16,8 @@ func updateMessage(s string) {
 func printMessage() {
 	fmt.Println(msg)
 }
+
+var wg sync.WaitGroup
 
 func main() {
 
@@ -25,12 +29,19 @@ func main() {
 
 	msg = "Hello, world!"
 
+	wg.Add(1)
 	go updateMessage("Hello, universe!")
+	wg.Wait()
 	printMessage()
 
+	wg.Add(1)
 	go updateMessage("Hello, cosmos!")
+	wg.Wait()
 	printMessage()
 
+	wg.Add(1)
 	go updateMessage("Hello, world!")
+	wg.Wait()
 	printMessage()
+
 }
