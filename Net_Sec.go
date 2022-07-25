44f09a080c00351d1f9e899b01af8545abbaf50b
Areeba T. Shoaib <areebashoaib99@gmail.com>
2022-07-26 01:00:42 +0200

    Starting from SCRATCH

--- Net_Sec.go
+++ Net_Sec.go
@@ -1,52 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"net"
-	"time"
-)
-
-//Created a structure
-type Packet struct {
-	payload byte
-	type1    int
-}
-
-//ability to write data  in the channel
-func BestEffort(payload byte, type1 int, ch chan<- Packet) {
-	for i := 1; i <= 10; i++ {
-		ch <- Packet{payload, type1} //Sending 10 best effort packets to channel;
-
-	}
-
-}
-func Reservation(payload byte, type1 int, ch chan<- Packet) {
-
-	for i := 1; i <= 10; i++ {
-		ch <- Packet{payload, type1} //Sending 10 Reservation Packets to channel;
-
-	}
-}
+import "fmt"
 
 func main() {
-	var p1 Packet
-	results := make(chan Packet)
-
-	If p1.type1==0{
-		go BestEffort(ch)
-	} else if p1.type1==1{
-		go Reservation(ch)
-	}
-
-
-
-
-
-
-
-
-
-
-
+	fmt.Println("YOU CAN DO IT!")
 
 }
