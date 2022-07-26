24df65f94fd5dc6a71fc935798fd5ae3de6e13dc
Areeba T. Shoaib <areebashoaib99@gmail.com>
2022-07-26 11:52:58 +0200

    Added functions

--- Net_Sec.go
+++ Net_Sec.go
@@ -1,8 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 type packet struct {
 	payload byte
 	type1   int
+}
+
+func checktype() {
+	rangeLower := 0
+	rangeUpper := 1
+	randomNum := rangeLower + rand.Intn(rangeUpper-rangeLower+1)
+	fmt.Println(randomNum)
+
+	for i := 0; i <= 5; i++ {
+		if randomNum == 0 {
+			//go to best effort function
+		} else {
+			//go to reservation function
+		}
+	}
 }
 
 func main() {
